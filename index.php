a8e6c4cd858a21012353c5d26f5eec44e730d244
kaplego <70806521+kaplego@users.noreply.github.com>
2022-04-12 01:03:01 +0200

    Added files
    - index.php
    - CSS (index, style)
    - Roboto Font

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Status Page</title>
+
+    <link rel="stylesheet" href="/assets/css/index.css">
+</head>
+<body>
+    <header>
+        <div id="header">
+            <div id="global-s-container">
+                <p id="title">
+                    <!-- Yes, this is a Subnautica reference -->
+                    Welcome aboard,<br>
+                    All systems <online>online</online>.
+                </p>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
